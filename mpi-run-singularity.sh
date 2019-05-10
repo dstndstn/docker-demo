80cc4b0c0d9dfb4dac1ca0e1939b207afc55ff2a
Dustin Lang <dstndstn@gmail.com>
2019-05-10 17:32:04 -0400

    give the mpi project it a different singularity img file

--- mpi-run-singularity.sh
+++ mpi-run-singularity.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 module load singularity
 module load mpich
-singularity run myproject.sif python3 /myproject/sampler.py --mpi
+singularity run myproject-mpi.sif python3 /myproject/sampler.py --mpi
